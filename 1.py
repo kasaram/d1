5e62a6109d6fff7e610661c8da257b7f68c518aa
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-08-24 17:08:43 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -21,14 +21,20 @@ def main(input_file, date_val, output_file):
         # Execute the query
         filtered_df = spark.sql(sql_query)
 
+        # Get the number of records in filtered_df
+        num_records = filtered_df.count()
+
         # Write the filtered DataFrame to a CSV file
         filtered_df.write.csv(output_file, header=True, mode="overwrite")
 
         # Stop the Spark session
         spark.stop()
 
-        # Custom success message
-        print("Spark job completed successfully!")
+        # Custom success message with the number of records
+        print(f"Spark job completed successfully! Number of records: {num_records}")
+
+        # Return the number of records for XCom
+        return num_records
 
     except Exception as e:
         # Custom failure message
@@ -44,11 +50,13 @@ if __name__ == '__main__':
     date_val = sys.argv[2]
     output_file = sys.argv[3]
 
-    main(input_file, date_val, output_file)
+    num_records = main(input_file, date_val, output_file)
 
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import XCom
 from datetime import datetime, timedelta
 
 default_args = {
@@ -74,14 +82,9 @@ input_file = "s3://file/pd_file.csv"
 date_val = "12-03-2023"
 output_file = "s3://file/output.csv"
 
-# Define the ssh command
-ssh_command = (
-    f"ssh -o StrictHostKeyChecking=no -t -i {KEY_FILE} hadoop@{MASTER_IP} "
-    f"spark-submit /home/hadoop/code.py {input_file} {date_val} {output_file}"
-)
-
 def on_success_callback(context):
-    print("Task succeeded. Spark job completed successfully!")
+    num_records = context['task_instance'].xcom_pull(task_ids='run_spark_job')
+    print(f"Task succeeded. Spark job completed successfully with {num_records} records!")
 
 def on_failure_callback(context):
     print("Task failed. Spark job failed. Check logs for details.")
@@ -93,6 +96,20 @@ run_spark_job = BashOperator(
     on_failure_callback=on_failure_callback,
     dag=dag,
 )
+
+def get_num_records(**kwargs):
+    ti = kwargs['ti']
+    num_records = ti.xcom_pull(task_ids='run_spark_job')
+    return num_records
+
+get_records_task = PythonOperator(
+    task_id='get_records_task',
+    python_callable=get_num_records,
+    provide_context=True,
+    dag=dag,
+)
+
+run_spark_job >> get_records_task
 
 if __name__ == "__main__":
     dag.cli()
