627494aaab2d630a10c12d47a40cd7e64afc4477
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-08-25 12:07:25 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,66 +1,55 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import XCom
-from datetime import datetime, timedelta
+from pyspark.sql import SparkSession
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2023, 8, 17),  # Modify with the actual start date
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
+def main(input_file, date_val, output_file):
+    try:
+        # Initialize Spark session
+        spark = SparkSession.builder.appName("AthenaQuery").getOrCreate()
 
-dag = DAG(
-    "spark_job_dag",
-    default_args=default_args,
-    schedule_interval=None,
-    catchup=False,
-    tags=["example"],
-)
+        # Create a DataFrame from the CSV file
+        df = spark.read.csv(input_file, header=True, inferSchema=True)
 
-# Modify these values accordingly
-MASTER_IP = "your_emr_master_ip"
-KEY_FILE = "/path/to/your/key_file.pem"
-input_file = "s3://file/pd_file.csv"
-date_val = "12-03-2023"
-output_file = "s3://file/output.csv"
+        # Create a temporary table from the DataFrame
+        df.createOrReplaceTempView("athena_table_name")
 
-# Define the ssh command
-ssh_command = (
-    f"ssh -o StrictHostKeyChecking=no -t -i {KEY_FILE} hadoop@{MASTER_IP} "
-    f"spark-submit /home/hadoop/code.py {input_file} {date_val} {output_file}"
-)
+        # Define the SQL query with "last_grading_date"
+        sql_query = """
+            SELECT *
+            FROM athena_table_name
+            WHERE last_grading_date = '{}'
+        """.format(date_val)
 
-def on_success_callback(context):
-    num_records = context['task_instance'].xcom_pull(task_ids='get_records_task')
-    print(f"Task succeeded. Spark job completed successfully with {num_records} records!")
+        # Execute the query
+        filtered_df = spark.sql(sql_query)
 
-def on_failure_callback(context):
-    print("Task failed. Spark job failed. Check logs for details.")
+        # Get the number of records in filtered_df
+        num_records = filtered_df.count()
 
-run_spark_job = BashOperator(
-    task_id="run_spark_job",
-    bash_command=ssh_command,
-    on_success_callback=on_success_callback,
-    on_failure_callback=on_failure_callback,
-    dag=dag,
-)
+        # Write the filtered DataFrame to a CSV file
+        filtered_df.write.csv(output_file, header=True, mode="overwrite")
 
-def get_num_records(**kwargs):
-    ti = kwargs['ti']
-    num_records = ti.xcom_pull(task_ids='run_spark_job')
-    return num_records
+        # Stop the Spark session
+        spark.stop()
 
-get_records_task = PythonOperator(
-    task_id='get_records_task',
-    python_callable=get_num_records,
-    provide_context=True,
-    dag=dag,
-)
+        # Custom success message with the number of records
+        print(f"Spark job completed successfully! Number of records: {num_records}")
 
-run_spark_job >> get_records_task
+        # Return the number of records for XCom
+        return num_records
 
-if __name__ == "__main__":
-    dag.cli()
+    except Exception as e:
+        # Custom failure message
+        print("An error occurred:", e)
+        # Return None in case of failure
+        return None
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: code.py <input_file> <date_val> <output_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    date_val = sys.argv[2]
+    output_file = sys.argv[3]
+
+    num_records = main(input_file, date_val, output_file)
