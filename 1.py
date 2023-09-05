416e2aabfbc9ae34c2dab53cacda8e060ce86df1
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-09-05 17:28:44 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,8 +8,8 @@ validations = input_df.rdd.map(validate_record)
 correct_records = validations.filter(lambda x: x[1] == "").map(lambda x: x[0])
 error_records = validations.filter(lambda x: x[1] != "").map(lambda x: x[0])
 
-# Create a DataFrame with a column for error messages
-error_records = error_records.map(lambda x: x + (lit(x[1]),)).toDF(["sl_no", "cis_code", "enitity_name", "model_name", "model_version", "segment", "definitive_grade", "definitive_pd", "cascade_flag", "country_of_operations", "last_grading_date", "error"])
+# Create a DataFrame for error records with an additional column for error messages
+error_records = error_records.map(lambda x: x + (x[1],)).toDF(["sl_no", "cis_code", "enitity_name", "model_name", "model_version", "segment", "definitive_grade", "definitive_pd", "cascade_flag", "country_of_operations", "last_grading_date", "error"])
 
 # Save the correct and error records to separate files
 correct_records.write.csv(correct_output_path, header=True, mode="overwrite")
