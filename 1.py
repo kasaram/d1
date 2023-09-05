76a9ea576c2be54ed7ee0ade51505640b3999727
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-09-05 17:48:25 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when
+from pyspark.sql.functions import col, when, lit
 from pyspark.sql.window import Window
-from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType, DoubleType
 from datetime import datetime
 
@@ -25,30 +24,23 @@ date_format_udf = udf(lambda date_str: datetime.strptime(date_str, "%d%m%Y").str
 def validate_cis_code(cis_code):
     return (cis_code is not None) and cis_code.isalnum()
 
-def validate_definitive_pd(pd, mgs):
-    lookup_row = lookup_df.filter(lookup_df["mgs"] == mgs).first()
-    if lookup_row:
-        lower_bound = lookup_row["lower_bound"]
-        upper_bound = lookup_row["upper_bound"]
-        return (pd is not None) and (lower_bound <= pd <= upper_bound)
-    else:
-        return False
-
 # Define validation UDFs
 validate_cis_code_udf = udf(validate_cis_code)
 validate_definitive_pd_udf = udf(validate_definitive_pd, DoubleType())
 
 # Apply validations
-validations = input_df.withColumn("error", when(input_df["cis_code"].isNull(), "cis_code is empty")
-                                       .when(~input_df["cis_code"].rlike("^[a-zA-Z0-9]+$"), "cis_code is not alphanumeric")
-                                       .when((input_df["definitive_pd"] < 0) | (input_df["definitive_pd"] > 1), "definitive_pd is out of range")
-                                       .when((input_df["definitive_grade"] < 1) | (input_df["definitive_grade"] > 27), "definitive_grade is out of range")
-                                       .when(~input_df["cascade_flag"].isin("Y", "N"), "cascade_flag is invalid")
-                                       .when((input_df["last_grading_date"].cast("string").cast("date") == "1900-01-01") | (input_df["last_grading_date"].cast("string").cast("date").isNull()), "last_grading_date has an invalid format")
-                                       .when((input_df["model_name"] == "Funds") & (input_df["segment"].isNull()), "segment cannot be null when model_name is 'Funds'")
-                                       .when(~validate_cis_code_udf(input_df["cis_code"]), "cis_code is not alphanumeric")
-                                       .when(~validate_definitive_pd_udf(input_df["definitive_pd"], input_df["definitive_grade"]), "definitive_pd is not within the range specified in lookup")
-                                       .otherwise(None))
+validations = input_df.join(lookup_df, input_df["definitive_grade"] == lookup_df["mgs"], "left") \
+    .withColumn("error", when(input_df["cis_code"].isNull(), "cis_code is empty")
+                          .when(~input_df["cis_code"].rlike("^[a-zA-Z0-9]+$"), "cis_code is not alphanumeric")
+                          .when((input_df["definitive_pd"] < 0) | (input_df["definitive_pd"] > 1), "definitive_pd is out of range")
+                          .when((input_df["definitive_grade"] < 1) | (input_df["definitive_grade"] > 27), "definitive_grade is out of range")
+                          .when(~input_df["cascade_flag"].isin("Y", "N"), "cascade_flag is invalid")
+                          .when((input_df["last_grading_date"].cast("string").cast("date") == "1900-01-01") | (input_df["last_grading_date"].cast("string").cast("date").isNull()), "last_grading_date has an invalid format")
+                          .when((input_df["model_name"] == "Funds") & (input_df["segment"].isNull()), "segment cannot be null when model_name is 'Funds'")
+                          .when(~validate_cis_code_udf(input_df["cis_code"]), "cis_code is not alphanumeric")
+                          .when(input_df["definitive_pd"].isNull(), "definitive_pd is empty")
+                          .when((lookup_df["mgs"].isNull()) | ((input_df["definitive_pd"] < lookup_df["lower_bound"]) | (input_df["definitive_pd"] > lookup_df["upper_bound"])), "definitive_pd is not within the range specified in lookup")
+                          .otherwise(None))
 
 # Split into correct and error records
 correct_records = validations.filter(validations["error"].isNull())
