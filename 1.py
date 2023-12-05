cd415ed36d2fc5eeb3a0c7b717eeff96803a0d59
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-12-05 19:29:56 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("UpdateRawInterface").getOrCreate()
+
+# Assuming correct_records and raw_interface are your DataFrames
+# Adjust column names accordingly
+
+# Assuming correct_records DataFrame
+correct_records = spark.table("correct_records")
+
+# Assuming raw_interface DataFrame
+raw_interface = spark.table("raw_interface")
+
+# Convert day_rk to date format for comparison
+raw_interface = raw_interface.withColumn("day_rk_date", F.to_date("day_rk", "dd-MM-yyyy"))
+
+# Define the max_day_rk_window
+max_day_rk_window = Window.partitionBy("counterparty_id").orderBy(F.desc("day_rk_date"))
+
 # Find the maximum day_rk value for each counterparty_id
 max_day_rk_df = (
     raw_interface
@@ -14,7 +36,7 @@ updated_raw_interface = (
         (raw_interface["counterparty_id"] == correct_records["cis_code"]) &
         (raw_interface["postcrm"] == correct_records["definitive_pd"]) &
         (raw_interface["precrm"] == correct_records["definitive_pd"]) &
-        (raw_interface["day_rk"] == correct_records["last_grading_date"]),
+        (raw_interface["day_rk_date"] == correct_records["last_grading_date"]),
         "left_outer"
     )
     .join(
@@ -28,5 +50,14 @@ updated_raw_interface = (
     .select(raw_interface["*"], correct_records["cis_code"].alias("updated_cis_code"))
 )
 
+# Drop temporary columns
+updated_raw_interface = updated_raw_interface.drop("day_rk_date", "max_day_rk")
+
 # Display the updated raw_interface
 updated_raw_interface.show()
+
+# Save the updated_raw_interface DataFrame to a new table or location
+# updated_raw_interface.write.mode("overwrite").saveAsTable("process.raw_interface_updated")
+
+# Stop the Spark session
+spark.stop()
