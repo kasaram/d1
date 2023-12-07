c22f02495b63ae8a7e16ca82ee5842dedd0a4253
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-12-07 12:01:06 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -37,7 +37,9 @@ raw_interface_new = (
     )
     .withColumn("updated_cis_code", F.coalesce(validated_records["cis_code"], raw_interface_new["updated_cis_code"]))
     .select(
-        raw_interface_new.columns,  # Retain all columns from raw_interface_new
+        "col1", "col2",  # Include all column names explicitly
+        "updated_cis_code",
+        "col3", "col4",  # Continue listing all column names
         F.when(validated_records["cis_code"].isNotNull(), 1).otherwise(0).alias("update_flag")
     )
 )
