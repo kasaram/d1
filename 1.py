315a40c834f2fc85576b4d8c7ae66032e2f82e21
kasaram Balasubrahmanyam <kasaram.dtc@gmail.com>
2023-12-13 21:57:54 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,51 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType
 
 # Initialize Spark session
 spark = SparkSession.builder.appName("UpdateRawInterface").getOrCreate()
 
-# Load raw_interface table from CSV file
-raw_interface = spark.read.csv("/path/to/raw_interface.csv", header=True, inferSchema=True)
+# Mockup data for raw_interface
+raw_interface_schema = StructType([
+    StructField("day_rk", StringType(), True),
+    StructField("counterparty_id", StringType(), True),
+    StructField("pd_score_postcrm", StringType(), True),
+    StructField("pd_score_precrm", StringType(), True),
+    StructField("col1", StringType(), True),
+    StructField("col2", StringType(), True),
+    # Add more dummy columns as needed
+])
 
-# Print the schema to identify the correct column names
-print("Schema of raw_interface:")
-raw_interface.printSchema()
+raw_interface_data = [
+    ("2023-01-01", "CIS001", "PD1", "PD1", "dummy1", "dummy2"),
+    ("2023-01-02", "CIS002", "PD2", "PD2", "dummy3", "dummy4"),
+    # Add more rows as needed
+]
+
+raw_interface = spark.createDataFrame(raw_interface_data, schema=raw_interface_schema)
+
+# Mockup data for correct_records
+correct_records_schema = StructType([
+    StructField("sl_no", StringType(), True),
+    StructField("cis_code", StringType(), True),
+    StructField("enitity_name", StringType(), True),
+    StructField("model_name", StringType(), True),
+    StructField("model_version", StringType(), True),
+    StructField("segment", StringType(), True),
+    StructField("definitive_grade", StringType(), True),
+    StructField("definitive_pd", StringType(), True),
+    StructField("cascade_flag", StringType(), True),
+    StructField("country_of_operations", StringType(), True),
+    StructField("last_grading_date", StringType(), True),
+])
+
+correct_records_data = [
+    ("1", "CIS001", "Entity1", "ModelA", "v1", "SegmentA", "GradeA", "PD1", "N", "CountryA", "2023-01-01"),
+    ("2", "CIS002", "Entity2", "ModelB", "v2", "SegmentB", "GradeB", "PD2", "N", "CountryB", "2023-01-02"),
+    # Add more rows as needed
+]
+
+correct_records = spark.createDataFrame(correct_records_data, schema=correct_records_schema)
 
 # Step 1: Print the total number of records in raw_interface
 total_records_before_update = raw_interface.count()
@@ -44,6 +80,7 @@ print(f'Total no of records in raw_interface_max_day_rk_data after updating coun
 
 # Step 7: Display only records with modified counterparty_id in raw_interface_max_day_rk_data
 modified_records = raw_interface_max_day_rk_data_after_update.filter("counterparty_id != cis_code")
+print("Modified Records:")
 modified_records.show(truncate=False)
 
 # Step 8: Create raw_interface_final by updating the original DataFrame
